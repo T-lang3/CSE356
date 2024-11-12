20d7de79951377bc5855e4d7b562cd52abf9ec43
root <root@milestone2.cloud.local>
2024-11-12 13:47:23 +0000

    videoprocessing.py is working but q being killed

--- ms2/python/video_processing.py
+++ ms2/python/video_processing.py
@@ -2,7 +2,7 @@
 import subprocess
 import os
 from pymongo import MongoClient
-#import movies from main.py
+
 from flask import Flask, render_template, request, jsonify, url_for, flash, redirect, session, send_file
 from flask_pymongo import PyMongo
 app = Flask(__name__)
@@ -10,10 +10,11 @@ def process_video(file_path, output_dir, movie_id):
     #from main import db
     print("entered the function2------------------------------------------------------------------")
     filename = os.path.splitext(os.path.basename(file_path))[0]
-    output_dir = "./media"
+    #output_dir = "./media"
 
-    print("filename: '"+str(filename)+"'")
-    print("file path: "+file_path+"\noutput: "+output_dir)
+    print("filename: '"+str(filename)+"'")                  #'videoplayback'
+    print("file path: "+file_path+"\noutput: "+output_dir)  # './static/upload/videoplayback.mp4'
+    print("stored path: "+f"{output_dir}/{filename}")       #'
     ffmpeg_command = [
         "ffmpeg", "-i", file_path,
         "-map", "0:v", "-b:v:0", "254k", "-s:v:0", "320x180",
@@ -25,16 +26,23 @@ def process_video(file_path, output_dir, movie_id):
         "-map", "0:v", "-b:v:6", "3134k", "-s:v:6", "1024x576",
         "-map", "0:v", "-b:v:7", "4952k", "-s:v:7", "1280x720",
         "-f", "dash", "-seg_duration", "10", "-use_template", "1", "-use_timeline", "1",
-        "-init_seg_name", f"{output_dir}/{filename}_$RepresentationID$_init.m4s",
-        "-media_seg_name", f"{output_dir}/{filename}_$Bandwidth$_$Number$.m4s",
+        "-init_seg_name", f"{filename}_$RepresentationID$_init.m4s",
+        "-media_seg_name", f"{filename}_$Bandwidth$_$Number$.m4s",
         "-adaptation_sets", "id=0,streams=v",
         f"{output_dir}/{filename}.mpd"
     ]
 
     subprocess.run(ffmpeg_command, check=True)
-
-    # MongoDB Update: Assuming the movie collection is already available
     
     db = PyMongo(app).db
     movies = db.movies
     movies.update_one({"id": movie_id}, {"$set": {"processed": "complete"}})
+
+
+# if __name__ == "__main__":
+#     # Example arguments
+#     file_path = "./static/upload/videoplayback.mp4"
+#     output_dir = "./media"
+#     movie_id = 1  # Replace with an actual movie ID
+
+#     process_video(file_path, output_dir, movie_id)

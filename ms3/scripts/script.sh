cd8b75b0ad0438bb6eaac7d51e0b24073d0c96ab
root <root@milestone3.cloud.local>
2024-11-24 19:40:02 +0000

    ssl done

--- ms3/scripts/script.sh
+++ ms3/scripts/script.sh
@@ -5,7 +5,7 @@ input_dir="../python/static/padded_videos"  # Change this to your video folder p
 output_dir="../python/media"  # Change this to your desired output folder path
 
 # Create the output directory if it doesn't exist
-mkdir -p "$output_dir"
+#mkdir -p "$output_dir"
 
 
 # pad=width=1280:height=720:x=-1:y=-1:color=black # change the width and height for everything
@@ -20,8 +20,8 @@ for video in "$input_dir"/*.mp4; do
         -map 0:v -b:v:1 768k -s:v:1 960x540 \
         -map 0:v -b:v:2 1024k -s:v:2 1280x720 \
         -f dash -seg_duration 10 -use_template 1 -use_timeline 1 \
-        -init_seg_name "${output_dir}/${filename}_\$RepresentationID\$_init.m4s" \
-        -media_seg_name "${output_dir}/${filename}_\$Bandwidth\$_\$Number\$.m4s" \
+        -init_seg_name "${filename}_\$RepresentationID\$_init.m4s" \
+        -media_seg_name "${filename}_\$Bandwidth\$_\$Number\$.m4s" \
         -adaptation_sets "id=0,streams=v" \
         "$output_dir/${filename}.mpd"
 
